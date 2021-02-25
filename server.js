163b750eb4ef34ad5e288268d6f4f6dddb84b2e9
arthurseredaa <neonbetcy@gmail.com>
2021-02-25 16:43:55 +0200

    Setup client

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 app.use("/graphql", graphqlHTTP({
   schema,
