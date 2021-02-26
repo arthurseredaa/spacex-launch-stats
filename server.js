0b2306417950eebdcadaf997a461ccf9d990c50f
arthurseredaa <neonbetcy@gmail.com>
2021-02-26 19:50:40 +0200

    Deploy to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
 const cors = require("cors");
+const path = require("path")
 
 const app = express();
 const PORT = process.env.PORT || 4000
@@ -12,6 +13,12 @@ app.use(cors());
 app.use("/graphql", graphqlHTTP({
   schema,
   graphiql: true
-}))
+}));
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+})
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
